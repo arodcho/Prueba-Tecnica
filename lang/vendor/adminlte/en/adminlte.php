8f0b7ab41f54164adda04d2142ff58fae1ee7587
arc0027 <104079189+arc0027@users.noreply.github.com>
2024-10-17 12:49:45 +0200

    Estructura e interfaz del proyecto

---
+++ lang/vendor/adminlte/en/adminlte.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    'full_name' => 'Nombre completo',
+'email' => 'Correo electrónico',
+'password' => 'Contraseña',
+'retype_password' => 'Reingresa la contraseña',
+'remember_me' => 'Recuérdame',
+'register' => 'Registrarse',
+'register_a_new_membership' => 'Registrar una nueva cuenta',
+'i_forgot_my_password' => 'Olvidé mi contraseña',
+'i_already_have_a_membership' => 'Ya tengo una cuenta',
+'sign_in' => 'Iniciar sesión',
+'log_out' => 'Cerrar sesión',
+'toggle_navigation' => 'Activar/desactivar navegación',
+'login_message' => 'Inicia sesión para comenzar tu sesión',
+'register_message' => 'Registrar una nueva cuenta',
+'password_reset_message' => 'Restablecer contraseña',
+'reset_password' => 'Restablecer contraseña',
+'send_password_reset_link' => 'Enviar enlace para restablecer contraseña',
+'verify_message' => 'Tu cuenta necesita ser verificada',
+'verify_email_sent' => 'Se ha enviado un nuevo enlace de verificación a tu correo electrónico.',
+'verify_check_your_email' => 'Antes de continuar, revisa tu correo para el enlace de verificación.',
+'verify_if_not_recieved' => 'Si no recibiste el correo',
+'verify_request_another' => 'haz clic aquí para solicitar otro',
+'confirm_password_message' => 'Por favor, confirma tu contraseña para continuar.',
+'remember_me_hint' => 'Mantenerme autenticado indefinidamente o hasta que cierre sesión manualmente',
+
+];
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/proyectos', [App\Http\Controllers\ProyectosController::class, 'index'])->name('proyectos');
+Route::get('/usuarios', [App\Http\Controllers\UsuariosController::class, 'index'])->name('usuarios');
+
